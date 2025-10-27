7cc22e80c5d34fb0a9e078ac8fc0caaefb3e42a2
delima-justine <delimajustine24@gmail.com>
2025-10-27 21:37:23 +0800

    add array type of Note in sidebar.ts

--- frontend/simple-app/src/app/sidebar/sidebar.ts
+++ frontend/simple-app/src/app/sidebar/sidebar.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Note } from '../../models/note.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,6 +11,7 @@ import { ReactiveFormsModule, FormGroup, FormControl, FormBuilder } from '@angul
 export class Sidebar {
   noteForm: FormGroup;
   formBuilder = inject(FormBuilder);
+  notes = signal(<Note[]>[]); 
 
   constructor() {
     this.noteForm = this.formBuilder.group({
