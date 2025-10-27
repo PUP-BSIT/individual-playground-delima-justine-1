acff3fcef72f76366f100fce5700b28f6e92e02f
delima-justine <delimajustine24@gmail.com>
2025-10-27 19:57:27 +0800

    add notes dashboard component.

---
+++ frontend/simple-app/src/app/notes-dashboard/notes-dashboard.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotesDashboard } from './notes-dashboard';
+
+describe('NotesDashboard', () => {
+  let component: NotesDashboard;
+  let fixture: ComponentFixture<NotesDashboard>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NotesDashboard]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NotesDashboard);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
