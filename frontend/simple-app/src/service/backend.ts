baf74281716a7833fe34fab8064ec27dbdbf76ac
delima-justine <delimajustine24@gmail.com>
2025-10-29 15:30:39 +0800

    services: add backend service

--- frontend/simple-app/src/service/backend.ts
+++ frontend/simple-app/src/service/backend.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { responseNote } from '../models/note.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Backend {
-  
+  http = inject(HttpClient);
+
+  fetchNotes() {
+    const url = `http://localhost:8080/notes`;
+    return this.http.get<responseNote>(url);
+  }
 }
