3a10bf85ee8374059467f5b39ec7a0eeb4dff883
delima-justine <delimajustine24@gmail.com>
2025-10-29 15:29:31 +0800

    add @CrossOrigin decorator to simple app controller backend which allow frontend requests.

--- backend/simple_app/src/main/java/com/simple/app/simple_app/controllers/SimpleAppController.java
+++ backend/simple_app/src/main/java/com/simple/app/simple_app/controllers/SimpleAppController.java
@@ -5,11 +5,12 @@ import com.simple.app.simple_app.models.Note;
 import com.simple.app.simple_app.repo.NoteRepo;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class SimpleAppController {
 
